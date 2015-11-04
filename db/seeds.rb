3433ff47ffbbab45e2bb2670a7d5430b5712bc30
Chip Self <chipself9@gmail.com>
2015-11-04 02:33:36 -0500

    fixed bad seed value

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: "Family Guy", description: "Family Guy.", small_cover_url: "/tmp/futurama.jpg" , large_cover_url: "/tmp/monk_large.jpg}")
+Video.create(title: "Family Guy", description: "Family Guy.", small_cover_url: "/tmp/futurama.jpg" , large_cover_url: "/tmp/monk_large.jpg")
 Video.create(title: "South Park", description: "A bunch of funny dudes.", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg")
 Video.create(title: "Family Guy", description: "King of the Jungle.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg")
