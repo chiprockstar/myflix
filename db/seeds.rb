485a331a18ed7e64dd85b7e8db81cec25b0c0995
Chip Self <chipself9@gmail.com>
2015-11-07 23:10:57 -0500

    re-seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,14 @@
 Video.create(title: "Futrama", description: "Futurama.", small_cover_url: "/tmp/futurama.jpg" , large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
 Video.create(title: "South Park", description: "A bunch of funny dudes.", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 2)
 Video.create(title: "Family Guy", description: "King of the Jungle.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 3)
+
+Video.create(title: "Futrama 2", description: "Futurama 2.", small_cover_url: "/tmp/futurama2.jpg" , large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create(title: "South Park 2", description: "A bunch of funny dudes 2.", small_cover_url: "/tmp/south_park2.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 2)
+Video.create(title: "Family Guy", description: "King of the Jungle.", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 3)
+
+Video.create(title: "Futrama 3", description: "Futurama 3.", small_cover_url: "/tmp/futurama3.jpg" , large_cover_url: "/tmp/monk_large.jpg", category_id: 1)
+Video.create(title: "South Park 3", description: "A bunch of funny dudes 3.", small_cover_url: "/tmp/south_park3.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 2)
+Video.create(title: "Family Guy 3", description: "King of the Jungle 3.", small_cover_url: "/tmp/family_guy3.jpg", large_cover_url: "/tmp/monk_large.jpg", category_id: 3)
 Category.create(category: "TV Commedies")
 Category.create(category: "TV Dramas")
 Category.create(category: "Reality TV")
